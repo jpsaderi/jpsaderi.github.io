066453f4171ff2c66c10a15c47e740dd05586e05
jpsaderi <jpsaderi@hotmail.com>
2024-11-23 16:09:28 -0300

    update: melhorias no código morse

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1718728403"
+    serviceWorkerVersion: "1957139508"
   }
 });
